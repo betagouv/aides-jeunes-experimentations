2c04855c9defe90cb1ee2a831056c043e933888b
Thomas Guillet <thomas@palya.fr>
2022-06-27 18:37:28 +0200

    Explicite et permet les différentes possibilités de préremplissage

--- pages/preremplissage.js
+++ pages/preremplissage.js
@@ -5,13 +5,16 @@ import data from './demo.json'
 
 export default function Preremplissage() {
   const [simulation, setSimulation] = useState()
+  const teleserviceName = "aides_jeunes_preremplissage"
 
-  const handleClick = (e) => {
+  const handleClick = (teleservice) => {
     let url = "https://mes-aides.1jeune1solution.beta.gouv.fr/api/simulation"
     url = "http://localhost:8080/api/simulation"
+    const body = {...data, ...(teleservice ? { teleservice: teleserviceName } : {})}
+    console.log('body', body)
     fetch(url, {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json"
       },
@@ -30,12 +33,28 @@ export default function Preremplissage() {
     })
   }
 
+  function generateRedirectURL(location) {
+    const to = location ? `&to=${location}` : ''
+    return `http://localhost:8080/api/simulation/${simulation._id}/redirect?token=${simulation.token}${to}`
+  }
+
   return (
     <>
-      <button onClick={handleClick}>GO</button>
+      <p>Les boutons permettent la génération de simulations à partir de données externes. 
+        Avec la redirection, la page de résultats n'est pas affichée. Les personnes sont 
+        directement renvoyées vers le site associé au téléservice indiqué (ici <code>{`${teleserviceName}`}</code>).
+      </p>
+      <button onClick={() => handleClick()}>GO</button>
+      <button onClick={() => handleClick(true)}>GO avec redirection</button>
       <div>
         { simulation && (
-          <a target="_blank" rel="noreferrer" href={`http://localhost:8080/api/simulation/${simulation._id}/redirect?token=${simulation.token}`}>Lien</a>
+          <>
+            <p>
+
+            </p>
+            <div><a target="_blank" rel="noreferrer" href={generateRedirectURL()}>Lien début</a></div>
+            <div><a target="_blank" rel="noreferrer" href={generateRedirectURL('/resultats')}>Lien page de résultats</a></div>
+          </>
           ) }
       </div>
       <pre>{JSON.stringify(simulation, null, 2)}</pre>
