d980f3e8c25a56e09d8bcdc9dacacd031a2f235b
Thomas Guillet <thomas@palya.fr>
2024-04-11 11:27:11 +0200

    Ajuste la config de déploiement GH pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProduction = 'production' === process.env.NODE_ENV
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: isProduction ? '/aides-jeunes-experimentations' : '',
   env: {
     commitSHA: process.env.GITHUB_SHA || "local",
   }
